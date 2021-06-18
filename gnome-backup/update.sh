30ddab31a959865a503374fd4421f4ca0da9f46b
Bastian <maxiqboy@gmail.com>
2021-06-18 17:46:20 +0700

    minor fix

--- gnome-backup/update.sh
+++ gnome-backup/update.sh
@@ -6,14 +6,14 @@ echo "================================================="
 
 echo "Update for Dash to Panel's setting"
 
-dconf load /org/gnome/shell/extensions/dash-to-panel/ < ./gnome-backup/dash-to-panel
+dconf load /org/gnome/shell/extensions/dash-to-panel/ < ./dash-to-panel
 
 echo "Update for Unite's setting"
 
-dconf load /org/gnome/shell/extensions/unite/ < ./gnome-backup/unite
+dconf load /org/gnome/shell/extensions/unite/ < ./unite
 
 echo "Update for keybindings setting"
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ./gnome-backup/keybindings
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ./keybindings
 
 
 echo "======== Restart the Gnome Shell! ========"
