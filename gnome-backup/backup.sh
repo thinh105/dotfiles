e3ba92e82a1053c3214cf88f9e393c0a308894ba
Bastian <maxiqboy@gmail.com>
2021-07-26 09:39:53 +0700

    update Dash To Panel's settings

--- gnome-backup/backup.sh
+++ gnome-backup/backup.sh
@@ -6,13 +6,13 @@ echo "================================================="
 
 echo "Backup for Dash to Panel's setting"
 
-dconf dump /org/gnome/shell/extensions/dash-to-panel/ > ./dash-to-panel
+dconf dump /org/gnome/shell/extensions/dash-to-panel/ > ./gnome-backup/dash-to-panel
 
 echo "Backup for Unite's setting"
 
-dconf dump /org/gnome/shell/extensions/unite/ > ./unite
+dconf dump /org/gnome/shell/extensions/unite/ > ./gnome-backup/unite
 
 echo "Backup for keybindings setting"
-dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > ./keybindings
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > ./gnome-backup/keybindings
 
 echo "======== Backup successfully!!! ========"
